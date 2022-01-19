e4d12162c1ccaef06ad6b61dc4ced3d3a2a7eef3
Meherazhossain91 <itsonlyopu@gmail.com>
2022-01-19 23:24:26 +0600

    fiverr order counter

---
+++ apps.js
@@ -0,0 +1,50 @@
+
+
+
+const fiverr = document.getElementById('fiverr');
+const counter = document.querySelector('.counter');
+
+
+
+fiverr.addEventListener('submit', function(e){
+
+e.preventDefault();
+
+ let date = (this.querySelector('input[type = "date"]').value);
+ let time = (this.querySelector('input[type = "time"]').value);
+
+ 
+ 
+
+
+ setInterval(() => {
+
+
+    let start_time = new Date();
+    let end_time = new Date(date + ' ' + time);
+   
+   
+   
+    let time_diff = Math.floor(Math.abs(end_time.getTime() - start_time.getTime()));
+   
+     
+   let total_sec = Math.floor(time_diff / 1000);
+   let total_min = Math.floor(total_sec / 60);
+   let total_hours = Math.floor(total_min / 60);
+   let total_day = Math.floor(total_hours / 24);
+   
+   
+   
+   let hours = total_hours - (total_day * 24);
+   let min = total_min - (total_day * 24 * 60) - (hours * 60);
+   let sec = total_sec - (total_day * 24 * 60 * 60) - (hours * 60 * 60) - (min * 60);
+
+
+   counter.innerHTML = `${total_day > 9 ? total_day: '0' +total_day}: ${hours > 9 ? hours: '0' +hours} : ${min > 9 ? min: '0' +min}: ${sec > 9 ? sec: '0' +sec}`;
+
+     
+ },1000 );
+
+ 
+
+});
